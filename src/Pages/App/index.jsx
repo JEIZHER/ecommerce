b7ecaec7875c0b1ec25a399e66cc96ffb0826729
Joseph Izarra <New.Jeizher@gmail.com>
2023-07-06 20:00:25 -0430

    Enrutamiento con React Router DOM

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,28 +1,30 @@
-import {Home} from '../Home'
-import {MyAccount} from '../MyAccount'
-import {MyOrder} from '../MyOrder'
-import {MyOrders} from '../MyOrders'
-import {NotFound} from '../NotFound'
-import {SignIn} from '../SignIn'
-import './App.css'
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import {Home}  from "../Home";
+import { MyAccount } from "../MyAccount";
+import { MyOrder } from "../MyOrder";
+import { MyOrders } from "../MyOrders";
+import { NotFound } from "../NotFound";
+import { SignIn } from "../SignIn";
+import "./App.css";
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {path: '/', element: <Home />, },
+    {path: '/MyAccount', element: <MyAccount/>,},
+    {path: '/MyOrder', element: <MyOrder />, },
+    {path: '/MyOrders', element: <MyOrders />, },
+    {path: '/SignIn', element: <SignIn />, },
+    {path: '/*', element: <NotFound />, },
+]);
+  return routes;
+};
 
-
+const App = () => {
   return (
-    
-   <>
-      <div className='bg-red-400'>
-        <Home/>
-        <MyAccount/>
-        <MyOrder/>
-        <MyOrders/>
-        <NotFound/>
-        <SignIn/>
-      </div>
-    </>
-    
-  )
-}
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
 
-export  {App}
+export { App };
