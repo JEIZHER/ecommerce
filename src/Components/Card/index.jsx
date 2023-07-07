8318567967979b7b7b95e120950b3e1e627595af
Joseph Izarra <New.Jeizher@gmail.com>
2023-07-07 19:19:35 -0430

    consumiendo la API para pintar cards

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,14 +1,14 @@
-const Card = () => {
+const Card = ({data}) => {
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-3 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 p-1">
-          Electronics
+          {data.category.name}
         </span>
         <img
           className="w-full h-full objet-cover rounded-lg"
-          src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Headphone"
+          src={data.images[0]}
+          alt={data.title}
         />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 px-3 py-0.5">
           +
@@ -16,11 +16,11 @@ const Card = () => {
       </figure>
 
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headphone</span>
-        <span className="text-lg font-medium">$300</span>
+        <span className="text-sm font-light">{data.title}</span>
+        <span className="text-lg font-medium">${data.price}</span>
       </p>
     </div>
   );
 };
 
-export { Card };
+export {Card};
