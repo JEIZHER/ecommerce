774a19b3f2df78072ee689566242d0e9312051de
Joseph Izarra <New.Jeizher@gmail.com>
2023-07-06 21:54:23 -0430

    Creando el componente Navbar y estilizando con taildwin

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,80 @@
+import {NavLink} from "react-router-dom";
+const NavBar = () => {
+    const activeStyle = 'underline underline-offset-4'
+  return (
+    <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-ligth">
+      <ul className="flex align-center gap-3">
+        <li className="font-semibold text-lg">
+          <NavLink 
+          to="/"
+          >Shopi</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/all"
+          className={({isActive})=> isActive? activeStyle:undefined}
+          >All</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/clothes"
+          className={({isActive})=> isActive? activeStyle:undefined}
+          >Clothes</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/electronics"
+          className={({isActive})=> isActive? activeStyle:undefined}
+          >Electronics</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/furnitures"
+          className={({isActive})=> isActive? activeStyle:undefined}
+          >Furnitures</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/toys"
+          className={({isActive})=> isActive? activeStyle:undefined}
+          >Toys</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/others"
+          className={({isActive})=> isActive? activeStyle:undefined}
+          >Others</NavLink>
+        </li>
+      </ul>
+
+      <ul className="flex align-center gap-3">
+        <li className="text-black/60">
+          <NavLink >New.jeizher@gmail.com</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/MyOrders"
+          className={({isActive})=> isActive? activeStyle:undefined}
+          >My Orders</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/MyAccount"
+          className={({isActive})=> isActive? activeStyle:undefined}
+          >My Account</NavLink>
+        </li>
+        <li>
+          <NavLink 
+          to="/SignIn"
+          className={({isActive})=> isActive? activeStyle:undefined}
+          >Sign In</NavLink>
+        </li>
+        <li>
+          <NavLink >♂ 1</NavLink>
+        </li>
+    </ul>
+    </nav>
+  );
+};
+
+export  {NavBar}
