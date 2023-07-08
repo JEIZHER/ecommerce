c8b42ce539ad710e39b22dbcef9e6f11a0a29d8f
Joseph Izarra <New.Jeizher@gmail.com>
2023-07-07 20:46:43 -0430

    Creando el Contexto global de la aplicación

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,32 +1,34 @@
 import { useRoutes, BrowserRouter } from "react-router-dom";
-import {Home}  from "../Home";
+import { ShoppingCartProvider } from "../../Context";
+import { Home } from "../Home";
 import { MyAccount } from "../MyAccount";
 import { MyOrder } from "../MyOrder";
 import { MyOrders } from "../MyOrders";
 import { NotFound } from "../NotFound";
 import { SignIn } from "../SignIn";
-import {NavBar} from "../../Components/NavBar"
+import { NavBar } from "../../Components/NavBar";
 import "./App.css";
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    {path: '/', element: <Home />, },
-    {path: '/MyAccount', element: <MyAccount/>,},
-    {path: '/MyOrder', element: <MyOrder />, },
-    {path: '/MyOrders', element: <MyOrders />, },
-    {path: '/SignIn', element: <SignIn />, },
-    {path: '/*', element: <NotFound />, },
-]);
+    { path: "/", element: <Home /> },
+    { path: "/MyAccount", element: <MyAccount /> },
+    { path: "/MyOrder", element: <MyOrder /> },
+    { path: "/MyOrders", element: <MyOrders /> },
+    { path: "/SignIn", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
   return routes;
 };
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <NavBar/>
-      <AppRoutes />
-      
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <NavBar />
+        <AppRoutes />
+      </BrowserRouter>
+    </ShoppingCartProvider>
   );
 };
 
